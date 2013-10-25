e2ce9d5b44676b56b775557743276b0145a7b723
Mark Berryman <markwberryman@yahoo.com>
2013-10-25 15:52:49 -0700

    switching to python's built-in mocking fx

--- LinkChecker/tests/test_linkTransformProcessor.py
+++ LinkChecker/tests/test_linkTransformProcessor.py
@@ -1,38 +1,44 @@
 import link
+import linkTransform
 import linkTransformProcessor
 import unittest
-
-
-class MockTransform(object):
-    def __init__(self):
-        self.transformFnCalled = False
-
-    def transform(self, currentLink, link):
-        self.transformFnCalled = True
+from unittest.mock import call
+from unittest.mock import MagicMock
 
 
 class LinkTransformProcessor_ApplyTransformersTests(unittest.TestCase):
-    def test_RaisesTypeErrorIfContextIsNone(self):
+    def test_RaisesTypeErrorIfProcessingContextIsNone(self):
         sut = linkTransformProcessor.LinkTransformProcessor(None)
 
         self.assertRaises(TypeError, sut.apply_transformers, None, "some link")
 
-    def test_RaisesTypeErrorIfNewLinksIsNone(self):
+    def test_RaisesTypeErrorIfLinksIsNone(self):
         sut = linkTransformProcessor.LinkTransformProcessor(None)
 
         self.assertRaises(TypeError, sut.apply_transformers, "some context", None)
 
-    def test_AppliesAllTransforms(self):
-        dummyTransformA = MockTransform()
-        dummyTransformB = MockTransform()
-        dummyTransformers = [dummyTransformA, dummyTransformB]
-        dummyLinks = [link.Link("a link")]
-        sut = linkTransformProcessor.LinkTransformProcessor(dummyTransformers)
+    def test_AppliesAllTransformsToAllLinks(self):
+        mock_transform_1 = linkTransform.LinkTransform()
+        mock_transform_2 = linkTransform.LinkTransform()
+        mock_transform_1.transform = MagicMock()
+        mock_transform_2.transform = MagicMock()
+        dummy_processing_context = dict()
+        link1 = link.Link("link1")
+        link2 = link.Link("link2")
+        dummy_links = set([link1, link2])
+        sut = linkTransformProcessor.LinkTransformProcessor(
+            [mock_transform_1, mock_transform_2])
 
-        sut.apply_transformers("current link", dummyLinks)
+        sut.apply_transformers(dummy_processing_context, dummy_links)
 
-        self.assertTrue(dummyTransformA.transformFnCalled)
-        self.assertTrue(dummyTransformB.transformFnCalled)
+        mock_transform_1.transform.assert_has_calls(
+            [call(dummy_processing_context, link1), 
+             call(dummy_processing_context, link2)], 
+             any_order=True)
+        mock_transform_2.transform.assert_has_calls(
+            [call(dummy_processing_context, link1), 
+             call(dummy_processing_context, link2)], 
+             any_order=True)
 
 
 if __name__ == '__main__':
