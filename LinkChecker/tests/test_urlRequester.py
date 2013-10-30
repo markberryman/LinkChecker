0791694f529582638da7b2c52a19eece83b5660c
Mark Berryman <markwberryman@yahoo.com>
2013-10-30 14:37:11 -0700

    moving connection creation into private method in UrlRequester; simplifying unit tests

--- LinkChecker/tests/test_urlRequester.py
+++ LinkChecker/tests/test_urlRequester.py
@@ -10,54 +10,44 @@ class UrlRequester_RequestUrlTests(unittest.TestCase):
 
         self.assertRaises(TypeError, sut.request_url, None)
 
-    def test_CreatesHttpConnectionWithProvidedUrlAndTimeout(self):
-        mock_url_parts = MagicMock()
-        mock_url_parts.path = "path"
+    def test_CreatesHttpConnectionWithProvidedUrl(self):
+        dummy_url_parts = "url parts"
         mock_url_parse_wrapper = MagicMock()
-        mock_url_parse_wrapper.parse_url = MagicMock(return_value=mock_url_parts)
-        mock_http_conn_wrapper = MagicMock()
-        dummy_url = "http://foo.com/path"
-        sut = urlRequester.UrlRequester(
-            mock_http_conn_wrapper, mock_url_parse_wrapper)
+        mock_url_parse_wrapper.parse_url = MagicMock(return_value=dummy_url_parts)
+        sut = urlRequester.UrlRequester(None, mock_url_parse_wrapper)
+        sut._create_http_conn = MagicMock()
 
-        sut.request_url(dummy_url)
+        sut.request_url("url")
 
-        mock_http_conn_wrapper.create_connection.assert_called_with(
-            mock_url_parts, 5)
+        sut._create_http_conn.assert_called_with(dummy_url_parts)
 
-    def test_MakesGetRequestWithuserAgentHeader(self):
-        mock_url_parts = MagicMock()
-        mock_url_parts.path = "path"
-        mock_url_parse_wrapper = MagicMock()
-        mock_url_parse_wrapper.parse_url = MagicMock(return_value=mock_url_parts)
-        mock_http_conn = MagicMock()
-        mock_http_conn_wrapper = MagicMock()
-        mock_http_conn_wrapper.create_connection = MagicMock(return_value=mock_http_conn)
-        sut = urlRequester.UrlRequester(
-            mock_http_conn_wrapper, mock_url_parse_wrapper)
-
-        sut.request_url("some url")
-
-        mock_http_conn.request.assert_called_with(
-            "GET", mock_url_parts.path, body=None, headers={ "User-Agent": "linkChecker" })
-        
+    # todo - should really test that we print/log something here
     def test_HandlesSocketErrorWhenGettingResponse(self):
-        mock_url_parts = MagicMock()
-        mock_url_parts.path = "path"
-        mock_url_parse_wrapper = MagicMock()
-        mock_url_parse_wrapper.parse_url = MagicMock(return_value=mock_url_parts)
         mock_http_conn = MagicMock()
         mock_http_conn.getresponse = MagicMock(side_effect=socket.error)
-        mock_http_conn_wrapper = MagicMock()
-        mock_http_conn_wrapper.create_connection = MagicMock(return_value=mock_http_conn)
         sut = urlRequester.UrlRequester(
-            mock_http_conn_wrapper, mock_url_parse_wrapper)
+            None, None)
+        sut._url_parse_wrapper = MagicMock()
+        sut._create_http_conn = MagicMock(return_value=mock_http_conn)
         expected = None
 
         actual = sut.request_url("some url")
 
         self.assertEqual(expected, actual)
+            
+    def test_ReturnsResponse(self):
+        dummy_response = "response"
+        mock_http_conn = MagicMock()
+        mock_http_conn.getresponse = MagicMock(return_value=dummy_response)
+        sut = urlRequester.UrlRequester(
+            None, None)
+        sut._url_parse_wrapper = MagicMock()
+        sut._create_http_conn = MagicMock(return_value=mock_http_conn)
+        expected = dummy_response
 
+        actual = sut.request_url("some url")
+
+        self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
     unittest.main()
--- LinkChecker/urlRequester.py
+++ LinkChecker/urlRequester.py
@@ -9,6 +9,17 @@ class UrlRequester(object):
         self._http_conn_wrapper = http_conn_wrapper
         self._url_parse_wrapper = url_parse_wrapper
 
+    def _create_http_conn(self, urlParts):
+        # need to include some user agent value otherwise
+        # sites are rejecting the request
+        headers = {"User-Agent": "linkChecker"}
+        
+        conn = self._http_conn_wrapper.create_connection(urlParts, self.connTimeout)
+        
+        conn.request("GET", urlParts.path, body=None, headers=headers)
+
+        return conn
+
     def request_url(self, url):
         """Requests a url and returns an HTTPResponse object."""
         result = None
@@ -17,14 +28,8 @@ class UrlRequester(object):
             raise TypeError("URL can not be none.")
 
         urlParts = self._url_parse_wrapper.parse_url(url)
-
-        # need to include some user agent value otherwise
-        # sites are rejecting the request
-        headers = {"User-Agent": "linkChecker"}
-
-        conn = self._http_conn_wrapper.create_connection(urlParts, self.connTimeout)
-
-        conn.request("GET", urlParts.path, body=None, headers=headers)
+                
+        conn = self._create_http_conn(urlParts)
 
         try:
             result = conn.getresponse()
