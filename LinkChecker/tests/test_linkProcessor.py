1d894d668707d35da8ccd33633f9bd9337b1fa68
Mark Berryman <markwberryman@yahoo.com>
2013-10-31 09:32:15 -0700

    renaming unit test class

--- LinkChecker/tests/test_linkProcessor.py
+++ LinkChecker/tests/test_linkProcessor.py
@@ -3,7 +3,7 @@ import unittest
 
 
 class LinkProcessor_ProcessLinkTests(unittest.TestCase):
-    def test_RaisesTypeErrorIfLinkToProcessIsNone(self):
+    def test_RaisesTypeErrorIfLinkRequestResultIsNone(self):
         sut = linkProcessor.LinkProcessor(None, None, None)
 
         self.assertRaises(TypeError, sut.process_link, None)
