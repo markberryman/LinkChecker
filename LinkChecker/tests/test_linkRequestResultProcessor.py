0a98c50928648f1e9f6baca6257e2324e4c42129
Mark Berryman <markwberryman@yahoo.com>
2013-11-12 14:11:31 -0800

    +test

--- LinkChecker/tests/test_linkRequestResultProcessor.py
+++ LinkChecker/tests/test_linkRequestResultProcessor.py
@@ -35,7 +35,22 @@ class LinkRequestResultProcessor_ProcessLinkRequestResultTests(unittest.TestCase
         actual = invalid_markup_links.pop()
 
         self.assertEqual(expected, actual)
-        
+    
+    def test_ReturnsGoodLinksLinks(self):
+        mock_link_request_result = MagicMock()
+        mock_link_request_result.status_code = http.client.OK
+        mock_link_request_result.response = "response"
+        mock_link_request_results = [mock_link_request_result]
+        mock_link_processor = MagicMock()
+        mock_link_processor.process_link = MagicMock(return_value=["link1", "link2"])
+        sut = linkRequestResultProcessor.LinkRequestResultProcessor(mock_link_processor)
+        expected = set().union(set(["link1", "link2"]))
+
+        good_links, invalid_markup_links, broken_links = sut.process_link_request_result(mock_link_request_results)
+        actual = good_links
+
+        self.assertEqual(expected, actual)
+            
 
 if __name__ == '__main__':
     unittest.main()
