c58804d3af53c4529eebddd5b1949ac03f717e45
Mark Berryman <markwberryman@yahoo.com>
2013-11-15 19:30:43 -0800

    dropping properties

--- LinkChecker/linkrequest/linkRequestResult.py
+++ LinkChecker/linkrequest/linkRequestResult.py
@@ -1,28 +1,12 @@
 class LinkRequestResult(object):
     """Represents result of requesting a link."""
     def __init__(self, link_url, status_code, response, location_header):
-        self.__link_url = link_url
-        self.__status_code = status_code
-        self.__response = response
-        self._location_header = location_header
+        self.link_url = link_url
+        self.status_code = status_code
+        self.response = response
+        self.location_header = location_header
 
     def equals(self, other):
         return ((self.link_url == other.link_url) and
                 (self.status_code == other.status_code) and
                 (self.response == other.response))
-
-    @property
-    def link_url(self):
-        return self.__link_url
-
-    @property
-    def response(self):
-        return self.__response
-
-    @property
-    def status_code(self):
-        return self.__status_code
-
-    @property
-    def location_header(self):
-        return self._location_header
