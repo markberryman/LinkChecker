dec8333b84716cfae4197aab947e1e84e18aa5bd
Mark Berryman <markwberryman@yahoo.com>
2013-11-15 19:29:47 -0800

    dropping properties

--- LinkChecker/linkrequest/linkRequest.py
+++ LinkChecker/linkrequest/linkRequest.py
@@ -2,13 +2,5 @@ class LinkRequest(object):
     """Represents a unit of work for requesting a link with
     additional metadata controlling handling the request."""
     def __init__(self, link_url, read_response):
-        self._link_url = link_url
-        self._read_response = read_response
-
-    @property
-    def link_url(self):
-        return self._link_url
-
-    @property
-    def read_response(self):
-        return self._read_response
+        self.link_url = link_url
+        self.read_response = read_response
